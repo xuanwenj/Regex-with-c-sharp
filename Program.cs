77bf3d7b5a8235f9eb787172bd302e3989927315
xuanwenj <wenjingxuan2@gmail.com>
2024-09-08 18:02:48 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System;
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Add("12,2"));
+      
+        Console.ReadLine();
+
+    }
+
+    private static int Add(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            return 0;
+        }
+
+        string[] numbers = s.Split(',');
+
+        int sum = 0;
+
+        foreach (string n in numbers)
+          {
+            sum += int.Parse(n);
+        }
+
+        return sum;
+
+    }
+
+}
